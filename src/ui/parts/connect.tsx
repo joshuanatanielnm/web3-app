55d4b41cb97dd62cfbd0bbd94f425a36aeb1baa3
Joshua Nathaniel M <clsjoshua@gmail.com>
2022-06-29 17:24:19 +0700

    feat: update connect part

--- src/ui/parts/connect.tsx
+++ src/ui/parts/connect.tsx
@@ -1,12 +1,17 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useMemo } from 'react'
 import { useAccount } from 'wagmi'
 import { WalletSelector } from './wallet-selector'
 import { SignMessage } from './sign-message'
-import { Account } from './Account'
+import { Account } from './account'
+import { useMounted } from '../../hooks/useMounted'
+
+export const USER_VERIFIED_KEY = 'userVerified'
 
 export const Connect = () => {
   const { data: accountData } = useAccount()
-  const [isAvailable, setIsAvailable] = useState(false)
+  const { hasMounted } = useMounted()
+
+  const isAccountDataAvailable = hasMounted && accountData
   const [userVerified, setUserVerified] = useState<string>((): string => {
     const isVerified =
       typeof window !== 'undefined' ? localStorage.getItem('userVerified') : ''
@@ -16,6 +21,14 @@ export const Connect = () => {
       return ''
     }
   })
+
+  const isVerified = useMemo(() => {
+    const isBrowser = typeof window !== 'undefined'
+    if (userVerified && isBrowser) {
+      return Boolean(localStorage.getItem(USER_VERIFIED_KEY))
+    }
+    return ''
+  }, [userVerified])
 
   useEffect(() => {
     if (accountData) {
@@ -30,17 +43,13 @@ export const Connect = () => {
     }
   }, [accountData, userVerified])
 
-  useEffect(() => {
-    if (accountData) {
-      setIsAvailable(true)
-    } else {
-      setIsAvailable(false)
-    }
-  }, [accountData])
-
-  if (accountData && isAvailable && !userVerified)
-    return <SignMessage setUserVerified={setUserVerified} />
-  else if (accountData && isAvailable && userVerified) return <Account />
+  if (isAccountDataAvailable) {
+    return isVerified ? (
+      <Account />
+    ) : (
+      <SignMessage setUserVerified={setUserVerified} />
+    )
+  }
 
   return <WalletSelector />
 }
